f41f4dc4c0a60557be55b6d4db36350aeb93f9db
alexcos20 <alex.coseru@gmail.com>
2023-07-25 09:49:12 +0300

    Bump version: 0.0.9 → 0.0.10

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     url="https://github.com/oceanprotocol/pdr-utils",
     # fmt: off
     # bumpversion needs single quotes
-    version='0.0.9',
+    version='0.0.10',
     # fmt: on
     zip_safe=False,
 )
